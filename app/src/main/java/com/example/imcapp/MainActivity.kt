5aab6e0860a630107df43d838b4d4b26f74b6188
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-13 22:23:18 -0300

    Final

--- app/src/main/java/com/example/imcapp/MainActivity.kt
+++ app/src/main/java/com/example/imcapp/MainActivity.kt
@@ -1,37 +1,46 @@
 package com.example.imcapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
-import java.util.Locale
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.activity.ComponentActivity
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : ComponentActivity() {
+
+    lateinit var cxHomem : RadioButton
+    lateinit var cxMulher: RadioButton
+    lateinit var calcImc : Button
+    lateinit var altura : EditText
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        altura = findViewById(R.id.txtAltura)
+        cxHomem = findViewById<RadioButton>(R.id.checkboxHomem)
+        cxMulher = findViewById<RadioButton>(R.id.checkboxMulher)
+        calcImc = findViewById(R.id.button)
 
-        solicitarInformacoes()
-    }
 
-    private fun solicitarInformacoes() {
-        println("Por favor, informe seu sexo (M para masculino, F para feminino): ")
-        val sexo = readLine()?.toUpperCase()
-
-        println("Por favor, informe sua altura em metros (ex: 1.75): ")
-        val altura = readLine()?.toDouble()
-
-        if (sexo == "M" || sexo == "F") {
-            val pesoIdeal = calcularPesoIdeal(sexo, altura)
-            println("Seu peso ideal é: $pesoIdeal kg")
-        } else {
-            println("Sexo inválido.")
+        calcImc.setOnClickListener{
+            var mensagem = " "
+            val alt = altura.text.toString().toDouble()
+            var imcH = (72.7 *alt) - 58;
+            var imcM =(62.1  *alt) - 44.7;
+            if (cxHomem.isChecked){
+                mensagem = "O peso ideal é ${imcH}"
+                Toast.makeText(this, mensagem, Toast.LENGTH_LONG).show()
+            }else if (cxMulher.isChecked){
+                mensagem = "O peso ideal é ${imcM}"
+                Toast.makeText(this,mensagem,Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(this, "Defina o sexo", Toast.LENGTH_LONG).show()
+            }
         }
-    }
 
-    private fun calcularPesoIdeal(sexo: String?, altura: Double?): Double {
-        return when (sexo) {
-            "M" -> altura?.let { 72.7 * it - 58.0 } ?: 0.0
-            "F" -> altura?.let { 62.1 * it - 44.7 } ?: 0.0
-            else -> 0.0
-        }
     }
 }
